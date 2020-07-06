28036e873846388805d5ecefdfb9ea69daab979f
Pat <pzdanko@myseneca.ca>
2020-07-06 18:52:38 -0400

    Email functionality and dashboard page

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 
-//require('dotenv').config({path:"./config/keys.env"});
+require('dotenv').config({path:"./config/keys.env"});
 
 const meals = require("./model/meals")
 
@@ -47,6 +47,12 @@ app.get("/registration", (req, res) => {
     })
 });
 
+app.get("/dashboard", (req, res) => {
+    res.render("dashboard", {
+        title: "Dashboard"
+    })
+});
+
 app.post("/userLogin", (req, res) => {
 
     const logErrors = [];
@@ -58,10 +64,12 @@ app.post("/userLogin", (req, res) => {
         logErrors.push("You must enter a password.")
     }
 
-    if (logErrors.length > 0){
+    if (logErrors.length > 0) {
         res.render("login", {
             title: "Login",
-            errors: logErrors
+            errors: logErrors,
+            email: req.body.logEmail,
+            psw: req.body.logpsw
         })
     } else {
         res.redirect('/');
@@ -87,22 +95,47 @@ app.post("/userRegister", (req, res) => {
         if (req.body.regpsw.length > 12 || req.body.regpsw.length < 6) {
             regErrors.push("Password must be between 6 to 12 characters.");
         }
-        if (!req.body.regpsw.match("[0-9A-Za-z]+")){
-            regErrors.push("Password can only contain letters and numbers");
+        const regexLower = /[a-z]/g;
+        const regexUpper = /[A-Z]/g;
+        const regexNum = /[0-9]/g;
+        if (!req.body.regpsw.match(regexLower) || !req.body.regpsw.match(regexUpper) || !req.body.regpsw.match(regexNum)) {
+            regErrors.push("Password must contain an uppercase letter, a lowercase letter, and a number.");
         }
     }
 
-    if (regErrors.length > 0){
+    if (regErrors.length > 0) {
         res.render("registration", {
             title: "Registration",
-            errors: regErrors
+            errors: regErrors,
+            fname: req.body.fname,
+            lname: req.body.lname,
+            email: req.body.regEmail,
+            psw: req.body.regpsw
         })
     } else {
-        res.redirect("/");
+        const sgMail = require('@sendgrid/mail');
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+        const msg = {
+            to: `${req.body.regEmail}`,
+            from: 'pzdanko@myseneca.ca',
+            subject: 'Welcome to Valhalla Feasts!',
+            html: 
+            `<h2>Thank you for signing up ${req.body.fname} $${req.body.lname}!</h2>
+            <p>We're excited that this is working and don't have anything else to say!</p>
+            <p>Bye!</p>
+            <p>Yours truly, the Valhalla Feasts team</p>`
+        };
+        sgMail.send(msg)
+        .then(()=>{
+            res.redirect("/dashboard");;
+        })
+        .catch(err=>{
+            console.log(`Error ${err}`);
+        });
     }
 })
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log("Web Server is up and running");
 })
